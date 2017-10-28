c01a4eb3c2b1194d1dbe68fea82aa390850e2d41
Wilmer <vvilmervd@gmail.com>
2017-10-28 11:41:24 -0600

    taller1Prueba
    123

---
+++ Taller#1/PenalesGameTaller1/PenalesGameTaller1/Logica/player.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PenalesGameTaller1.Logica
+{
+    class player
+    {
+        //private int[] lanzamientosJug;
+        private List<int> lanzamientosPor = new List<int>();
+        private List<int> lanzamientosJug = new List<int>();
+        private int goles = 0;
+        private int paradas = 0;
+        private string golDerecha = "no hubieron goles por la derecha";
+        private string golCentro = "no hubieron goles por la centro";
+        private string golIzquierda = "no hubieron goles por la izquierda";
+        private int golesDerecha = 0;
+        private int golesIzquierda = 0;
+        private int golesCentro = 0;
+        private int turno = 0;
+        int porteroDireccion;
+
+        public string Juego(int disparoDireccion)
+        {
+
+            if (turno != 5)
+            {
+                Random random = new Random();
+                porteroDireccion = random.Next(1, 6);
+
+
+                lanzamientosJug.Add(disparoDireccion);
+                lanzamientosPor.Add(porteroDireccion);
+
+
+                if (disparoDireccion == porteroDireccion)
+                {
+                    paradas++;
+                    turno++;
+                    return "Keeeeyyylloooorrr";
+                }
+                else if (disparoDireccion == 3 || disparoDireccion == 4)
+                {
+                    goles++;
+                    golesIzquierda++;
+                    golIzquierda = "SI hubieron goles por la izquierda";
+                    turno++;
+                    return "Gooollllaaaaazzzoooo";
+                }
+                else if (disparoDireccion == 2 || disparoDireccion == 5)
+                {
+                    goles++;
+                    golesCentro++;
+                    golCentro = "SI hubieron goles por la centro";
+                    turno++;
+                    return "Gooollllaaaaazzzoooo";
+                }
+                else if (disparoDireccion == 1 || disparoDireccion == 6)
+                {
+                    goles++;
+                    golesDerecha++;
+                    golDerecha = "SI hubieron goles por la derecha";
+                    turno++;
+                    return "Gooollllaaaaazzzoooo";
+                }
+            } return "Juego Finalizado"+ "\n"+
+                "Direccion lazamientos jugador = "+ lanzamientosJug +"\n"+
+                "Direccion lazamientos portero = "+ lanzamientosPor + "\n" +
+                "Goles conseguidos = "+ goles+ "\n"+
+                "Penales fallados = " + paradas + "\n" +
+                "Goles por la derecha = " +golesDerecha+ "\n" +
+                "Goles por el centro = " + golesCentro + "\n" +
+                "Goles por la izquierda = " + golesIzquierda + "\n" +
+                golDerecha + "\n"+
+                golCentro + "\n" +
+                golIzquierda
+                ;
+        }
+
+    }
+}
+
+
---
+++ Taller#1/PenalesGameTaller1/PenalesGameTaller1/Vista/Form1.Designer.cs
@@ -0,0 +1,139 @@
+﻿namespace PenalesGameTaller1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btn1
+            // 
+            this.btn1.AccessibleName = "btn1";
+            this.btn1.Image = global::PenalesGameTaller1.Properties.Resources.bola;
+            this.btn1.Location = new System.Drawing.Point(475, 91);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(75, 72);
+            this.btn1.TabIndex = 0;
+            this.btn1.Text = "1";
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btn1_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.AccessibleName = "btn1";
+            this.btn2.Image = global::PenalesGameTaller1.Properties.Resources.bola;
+            this.btn2.Location = new System.Drawing.Point(268, 91);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(75, 72);
+            this.btn2.TabIndex = 1;
+            this.btn2.Text = "2";
+            this.btn2.UseVisualStyleBackColor = true;
+            this.btn2.Click += new System.EventHandler(this.btn2_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.AccessibleName = "btn1";
+            this.btn3.Image = global::PenalesGameTaller1.Properties.Resources.bola;
+            this.btn3.Location = new System.Drawing.Point(54, 91);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(75, 72);
+            this.btn3.TabIndex = 2;
+            this.btn3.Text = "3";
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btn3_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.AccessibleName = "btn1";
+            this.btn4.Image = global::PenalesGameTaller1.Properties.Resources.bola;
+            this.btn4.Location = new System.Drawing.Point(54, 235);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(75, 72);
+            this.btn4.TabIndex = 3;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btn4_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.AccessibleName = "btn1";
+            this.btn5.Image = global::PenalesGameTaller1.Properties.Resources.bola;
+            this.btn5.Location = new System.Drawing.Point(268, 235);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(75, 72);
+            this.btn5.TabIndex = 4;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btn5_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.AccessibleName = "btn1";
+            this.btn6.Image = global::PenalesGameTaller1.Properties.Resources.bola;
+            this.btn6.Location = new System.Drawing.Point(475, 235);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(75, 72);
+            this.btn6.TabIndex = 5;
+            this.btn6.Text = "6";
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btn6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::PenalesGameTaller1.Properties.Resources.arco1;
+            this.ClientSize = new System.Drawing.Size(574, 353);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+    }
+}
+
